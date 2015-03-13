69413fd742d9b3ff03cb33dfaadfc501206c9e03
SwaroopG <swarooprajg@gmail.com>
2015-03-13 16:15:24 -0500

    Using Apache loggers instead of standard out.

--- src/test/java/com/poorjar/controller/SearchControllerTest.java
+++ src/test/java/com/poorjar/controller/SearchControllerTest.java
@@ -1,16 +1,18 @@
 package com.poorjar.controller;
 
-import static com.jayway.restassured.RestAssured.expect;
 import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.equalTo;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
 
+import org.apache.log4j.Logger;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.IntegrationTest;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.boot.test.TestRestTemplate;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.web.client.RestTemplate;
@@ -27,21 +29,31 @@ import com.poorjar.entity.SearchItem;
 @IntegrationTest("server.port=9000")
 public class SearchControllerTest
 {
+    private static Logger LOGGER = Logger.getLogger(SearchControllerTest.class);
+
+    private static String baseUrl = "http://localhost:9000/";
     private static String searchUrl = "http://localhost:9000/search/description/swaroop/";
 
     RestTemplate template = new TestRestTemplate();
 
     @Test
-    public void testRequest() throws Exception
+    public void testMainRequest() throws Exception
     {
-        String body = template.getForEntity(searchUrl, String.class).getBody();
-        HttpStatus status = template.getForEntity(searchUrl, String.class).getStatusCode();
+        ResponseEntity<String> response = template.getForEntity(baseUrl, String.class);
 
-        System.out.println(body);
-        System.out.println(status.toString());
+        LOGGER.info("********Response" + response);
+        assertEquals(response.getStatusCode(), HttpStatus.OK);
+        assertThat(response.getBody(), equalTo("Hello Spring Boot!"));
+    }
 
-        assertEquals(status, HttpStatus.OK);
-        assertThat(body, containsString(getExpectedSearchResponse()));
+    @Test
+    public void testSearchRequest() throws Exception
+    {
+        ResponseEntity<String> response = template.getForEntity(searchUrl, String.class);
+
+        LOGGER.info("********Response" + response);
+        assertEquals(response.getStatusCode(), HttpStatus.OK);
+        assertThat(response.getBody(), containsString(getExpectedSearchResponse()));
     }
 
     private String getExpectedSearchResponse() throws Exception
